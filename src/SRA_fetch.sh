43c2475123f1593b3ae7aaf2105fa11ab75abd75
Morris Aguilar <MorrisGlr@users.noreply.github.com>
2020-11-02 16:26:31 -0500

    Add files via upload

---
+++ src/SRA_fetch.sh
@@ -0,0 +1,13 @@
+# range is the number of SRA assension numbers that represent individual samples.
+# adjust the range number according to the length of the SRA list
+# SRA_16s_list.txt, len = 670
+# SRA_metagenomics_list. txt, len = 50
+
+#!/bin/bash
+
+for i in {1..50}
+do
+SRA=$(sed -n $( echo $i )p SRA_metagenomics_list.txt)
+fastq-dump $SRA
+SRA=0
+done
