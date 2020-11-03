44b4277568c144fe81bdf79c71691f990a8dd055
Morris Aguilar <MorrisGlr@users.noreply.github.com>
2020-11-03 00:02:42 -0500

    Add files via upload
    removed duplicate SRAs

--- src/SRA_fetch.sh
+++ src/SRA_fetch.sh
@@ -1,6 +1,6 @@
 # range is the number of SRA assension numbers that represent individual samples.
 # adjust the range number according to the length of the SRA list
-# SRA_16s_list.txt, len = 670
+# SRA_16s_list.txt, len = 260
 # SRA_metagenomics_list. txt, len = 50
 
 #!/bin/bash
